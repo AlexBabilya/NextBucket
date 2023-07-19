ed11ea9fc2fdd91d28b08622e1b79d8b4c60ff3b
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-19 23:38:35 +0300

    Added: Refresh Token Logic

--- backend/routers.py
+++ backend/routers.py
@@ -1,13 +1,14 @@
 from rest_framework import routers
 
 from modules.user.viewsets import UserViewSet
-from modules.auth.viewsets import RegisterViewSet, LoginViewSet
+from modules.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
+router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
 
 urlpatterns = [
     *router.urls,
