ed2f4e4ada2ca9ef1b1453ac160c65f322956b85
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-19 14:54:49 +0300

    Added: Create User, Staff, SuperUser Methods in User Manager

--- backend/modules/user/models.py
+++ backend/modules/user/models.py
@@ -24,8 +24,40 @@ def user_directory_path(instance, filename):
 
 
 class UserManager(BaseUserManager, AbstractManager):
-    pass
-
+    def create_user(self, username, email, password=None, **kwargs):
+        if username is None:
+            raise TypeError('Users must have a username')
+        if email is None:
+            raise TypeError('Users must have an email')
+        if password is None:
+            raise TypeError('Users must have a password')
+        
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+            **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_stuff(self, username, email, password=None, **kwargs):
+        user = self.create_user(username, email, password, **kwargs)
+        
+        user.is_staff = True
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, username, email, password, **kwargs):
+        user = self.create_stuff(username, email, password, **kwargs)
+        
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+        return user           
+        
 
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(
@@ -45,14 +77,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     first_name = models.CharField(max_length=75)
     last_name = models.CharField(max_length=75)
-    email = models.EmailField(max_length=254, unique=True, db_index=True)
+    email = models.EmailField(max_length=254, unique=True, db_index=True,blank=False, 
+        null=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    height = models.DecimalField(max_digits=4, decimal_places=1)
-    weight = models.DecimalField(max_digits=4, decimal_places=1)
+    height = models.DecimalField(max_digits=4, decimal_places=1, default=0)
+    weight = models.DecimalField(max_digits=4, decimal_places=1, default=0)
     position = models.CharField(max_length=2, choices=POSITION_CHOICES, default=POSITION_CHOICES[0][0])
     city = models.CharField(max_length=255)
     region = models.CharField(max_length=255, blank=True, null=True)
@@ -60,10 +93,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     bio = models.TextField(null=True, blank=True)
     avatar = models.ImageField(null=True, blank=True, upload_to=user_directory_path)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
-    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, blank=True, null=True)
     
     EMAIL_FILED = 'email'
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
     
     objects = UserManager()
     
@@ -74,5 +108,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     def name(self):
         return f'{self.first_name} {self.last_name}'
     
-
-
