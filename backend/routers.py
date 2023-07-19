aa3b92cf952b0590a24ee4a6fb383a4234439fd5
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-19 23:30:47 +0300

    Added: Login Logic

--- backend/routers.py
+++ backend/routers.py
@@ -1,12 +1,13 @@
 from rest_framework import routers
 
 from modules.user.viewsets import UserViewSet
-from modules.auth.viewsets.register import RegisterViewSet
+from modules.auth.viewsets import RegisterViewSet, LoginViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls,
