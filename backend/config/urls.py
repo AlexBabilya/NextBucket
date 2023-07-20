fa29f36704d2e6af7e9cddcfb91b8f761e654f88
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-20 14:14:54 +0300

    Fixed: Router Import Bug and Password Hashing

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('routers')),
+    path('api/', include('modules.routers')),
 ]
--- backend/modules/auth/serializers/register.py
+++ backend/modules/auth/serializers/register.py
@@ -12,6 +12,9 @@ class RegisterSerializer(UserSerializer):
         write_only=True
     )
     
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
     class Meta:
         model = User
         fields = [
