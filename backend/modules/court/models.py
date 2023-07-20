bce341131b262908cab610b7deb644648b06ba52
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-20 14:57:02 +0300

    Added: Court Model

--- backend/modules/court/models.py
+++ backend/modules/court/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+from modules.abstract.models import AbstractManager
+
+def court_directory_path(instance, filename):
+    return f'court_{instance.public_id}/{filename}'
+
+class CourtManager(models.Manager):
+    pass
+
+
+class Court(models.Model):
+    public_id = models.UUIDField(
+        default=uuid.uuid4, 
+        editable=False,
+        unique=True,
+        blank=False,
+        null=False
+    )
+    name = models.CharField(
+        max_length=255, 
+        unique=True, 
+        db_index=True, 
+        blank=False, 
+        null=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    address = models.CharField(max_length=200,blank=True, null=True)
+    city = models.CharField(max_length=255,blank=True, null=True)
+    region = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to=court_directory_path)
+    latitude = models.CharField(max_length=200,blank=True, null=True)
+    longitude = models.CharField(max_length=200,blank=True, null=True)
+
+    objects = CourtManager()
+    
+    def __str__(self):
+        return self.name
+    
+    @property
+    def full_location(self):
+        return f'{self.address}, {self.city}, {self.region}, {self.country}'
+    
+    @property
+    def coordinates(self):
+        return f'{self.latitude}, {self.longitude}'
