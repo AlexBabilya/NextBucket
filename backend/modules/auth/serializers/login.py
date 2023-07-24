28c15ee263cd5d4aa5ad625d8b6f7acc4a8d965c
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-24 14:57:49 +0300

    Fixed: User Login by Username or Email

--- backend/modules/auth/serializers/login.py
+++ backend/modules/auth/serializers/login.py
@@ -1,19 +1,42 @@
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, ValidationError
 from rest_framework_simplejwt.settings import api_settings
 
 from django.contrib.auth.models import update_last_login
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 
 from modules.user.serializers import UserSerializer
+
+User = get_user_model()
 
 
 class LoginSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        data = super().validate(attrs)
+        self.user = None
+        username = attrs.get('username')
+        password = attrs.get('password')
+        
+        if username and password:
+            self.user = authenticate(request=self.context.get('request'),
+                                username=username, password=password)
+            if self.user is None:
+                try:
+                    self.user = User.objects.get(email=username)
+                    if self.user.check_password(password):
+                        return super().validate(attrs)
+                except User.DoesNotExist:
+                    pass
+        
+        if not self.user:
+            raise ValidationError('Unable to log in with provided credentials.')
+        
         refresh = self.get_token(self.user)
-        data["user"] = UserSerializer(self.user, context=self.context).data
-        data["refresh"] = str(refresh)
-        data["access"] = str(refresh.access_token)
 
         if api_settings.UPDATE_LAST_LOGIN:
             update_last_login(None, self.user)
-        return data
+            
+        return {
+            "user": UserSerializer(self.user, context=self.context).data,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token)
+        }
