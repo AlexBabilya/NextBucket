a52661d893c8538ec2ea32aa13a06824d3cb6486
OleksandrBabilia <djesssash1708@gmail.com>
2023-07-24 16:01:36 +0300

    Added: Added Court Rout

--- backend/modules/routers.py
+++ backend/modules/routers.py
@@ -1,11 +1,13 @@
 from rest_framework import routers
 
 from modules.user.viewsets import UserViewSet
+from modules.court.viewsets import CourtViewSet
 from modules.auth.viewsets import RegisterViewSet, LoginViewSet, RefreshViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'court', CourtViewSet, basename='court')
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
